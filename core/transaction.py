ef7392d51724890f313ad043e812aa5f3df18f0f
lzxd <ramy.badi98@gmail.com>
2025-06-13 22:21:42 +0200

    V0.000

--- core/transaction.py
+++ core/transaction.py
@@ -5,3 +5,11 @@ class Transaction:
         self.recipient = recipient
         self.amount = amount
         self.timestamp = time.time()
+
+    def __dict__(self):
+        return {
+            'sender': self.sender,
+            'recipient': self.recipient,
+            'amount': self.amount,
+            'timestamp': self.timestamp
+        }
