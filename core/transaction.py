66772b9f78d4aad40845d697fb005ec6de96276a
lzxd <ramy.badi98@gmail.com>
2025-06-13 22:12:01 +0200

    ggh

--- core/transaction.py
+++ core/transaction.py
@@ -1,6 +1,6 @@
 import time
 class Transaction:
-    def __init__(self, sender : str, recipient : str, amount : float):
+    def __init__(seelf, sender : str, recipient : str, amount : float):
         self.sender = sender
         self.recipient = recipient
         self.amount = amount
